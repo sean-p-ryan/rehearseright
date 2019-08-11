cb37a2539e8db1669768648355d47051463458cd
Sean Ryan <seanpryan89@gmail.com>
2019-08-11 17:58:48 -0400

    homepage view created

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 const routeConfig = require("./config/route-config.js");
+const appConfig = require("./config/main-config.js");
 
 routeConfig.init(app);
+appConfig.init(app, express);
 
 module.exports = app;
