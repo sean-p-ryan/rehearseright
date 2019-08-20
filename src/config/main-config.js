0f6f1052167f58720729c360c46879b25901b9b8
Sean Ryan <seanpryan89@gmail.com>
2019-08-19 20:49:33 -0400

    made minor changeS

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
 
+// here is some test code
+
 module.exports = {
     init(app, express) {
         app.set("views", viewsFolder);
