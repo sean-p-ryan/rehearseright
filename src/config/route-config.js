64b3ab9ba7241505f1baba27cf042d8b836c9ed8
Sean Ryan <ryan.sean@acmhq.acm.org>
2019-09-18 16:55:35 -0400

    working on user model

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,8 @@
 module.exports = {
     init(app) {
+        const userRoutes = require("../routes/users");
         const staticRoutes = require("../routes/static");
         app.use(staticRoutes);
+        app.use(userRoutes);
     }
 }
